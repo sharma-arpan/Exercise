7f87c24f1eb46c4c92a65976029e289d8a22ca2a
Arpan Sharma <arpansharma.aws@gmail.com>
2023-08-01 23:32:34 +0530

    error handelling

--- mydictionary.py
+++ mydictionary.py
@@ -6,20 +6,25 @@ import click
 API_KEY = os.environ.get("MERRIAM_WEBSTER_API_KEY")
 BASE_URL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"
 
+
 def get_word_definition(word):
     url = f"{BASE_URL}{word}?key={API_KEY}"
     response = requests.get(url)
-    if response.status_code == 200:
+    try:
+        response.raise_for_status()  # Check for HTTP errors (e.g., 404, 500)
         data = response.json()
         if isinstance(data, list):
             if data:
                 return data[0]['hwi']['hw'], data[0]['fl'], data[0]['shortdef'][0]
             else:
                 return None
-        else:
-            return None
-    else:
-        return None
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.RequestException as req_err:
+        print(f"Request error occurred: {req_err}")
+    except requests.exceptions.JSONDecodeError as json_err:
+        print(f"JSON decode error occurred: {json_err}")
+    return None
 
 @click.command()
 @click.argument("word")
