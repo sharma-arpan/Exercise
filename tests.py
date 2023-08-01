88e807d80d67cf62ad2e6cc80ffc99a579f83e8c
Arpan Sharma <arpansharma.aws@gmail.com>
2023-08-02 00:19:32 +0530

    debugging

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class DictionaryToolTests(unittest.TestCase):
     def test_get_word_definition(self):
         word = "exercise"
         print(get_word_definition(word))
-        pronunciation, word_type, definition = get_word_definition(word).split()
+        pronunciation, word_type, definition = get_word_definition(word)
         self.assertEqual(pronunciation, "ˈek-sər-ˌsīz")
         self.assertEqual(word_type, "noun")
         self.assertEqual(definition, "the act of bringing into play or realizing in action")
